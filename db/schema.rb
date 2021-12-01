b8ed7a538a6e2d60494a88959526be6eeb9d4838
arnold ho <tchoarnold@gmail.com>
2021-12-01 21:29:22 +0000

    sign in sign up forms

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_211109) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.integer "organisation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
